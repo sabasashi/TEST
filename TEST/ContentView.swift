59642afa1c71f06751559c59321b3c61e5e162c1
sabasashi <sabasashi@gmail.com>
2023-03-31 18:38:38 +0700

    Initial Commit

---
+++ TEST/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TEST
+//
+//  Created by 星野拓郎 on 2023/03/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TEST/TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESTApp.swift
+//  TEST
+//
+//  Created by 星野拓郎 on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
